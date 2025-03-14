f52fbad3932603c3064d4746b67ee714ffb3f8ae
Iggy <iggyf@iggys-air.home>
2025-03-15 00:19:37 +0100

    Ajout de l'animation du texte et du fond dynamique sur la page About

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect } from "react";
 import "../styles/Contact.css";
 import { FaGithub, FaEnvelope, FaLinkedin } from "react-icons/fa";
 
